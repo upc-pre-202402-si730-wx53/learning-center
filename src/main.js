c5ebdeacf574c1b3cd1d8acf36296f4ad5c5a987
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-09 12:12:55 -0500

    chore: set app instance initial configuration.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,77 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Vue Router
+import router from "./router/index.js";
+
+// PrimeVue
+import PrimeVue from 'primevue/config';
+
+// PrimeVue CSS
+import 'primeflex/primeflex.css';
+
+// PrimeVue Icons
+import 'primeicons/primeicons.css';
+import Toast from "primevue/toast";
+import Toolbar from "primevue/toolbar";
+import Card from "primevue/card";
+import Column from "primevue/column";
+import Button from "primevue/button";
+import ConfirmDialog from "primevue/confirmdialog";
+import Checkbox from "primevue/checkbox";
+import DataTable from "primevue/datatable";
+import Dialog from "primevue/dialog";
+import FileUpload from "primevue/fileupload";
+import FloatLabel from "primevue/floatlabel";
+import IconField from "primevue/iconfield";
+import InputIcon from "primevue/inputicon";
+import InputText from "primevue/inputtext";
+import InputNumber from "primevue/inputnumber";
+import Menu from "primevue/menu";
+import Rating from "primevue/rating";
+import Row from "primevue/row";
+import Tag from "primevue/tag";
+import Textarea from "primevue/textarea";
+import Drawer from "primevue/drawer";
+import Select from "primevue/select";
+import ConfirmationService from "primevue/confirmationservice";
+import DialogService from "primevue/dialogservice";
+import ToastService from "primevue/toastservice";
+
+// Default App Instance
+const app = createApp(App);
+
+// Router
+app.use(router);
+
+// PrimeVue
+app.use(PrimeVue, {ripple: true})
+    .use(ConfirmationService)
+    .use(DialogService)
+    .use(ToastService)
+    .component('pv-button',         Button)
+    .component('pv-card',           Card)
+    .component('pv-column',         Column)
+    .component('pv-confirm-dialog', ConfirmDialog)
+    .component('pv-checkbox',       Checkbox)
+    .component('pv-data-table',     DataTable)
+    .component('pv-dialog',         Dialog)
+    .component('pv-select',         Select)
+    .component('pv-file-upload',    FileUpload)
+    .component('pv-float-label',    FloatLabel)
+    .component('pv-icon-field',     IconField)
+    .component('pv-input-icon',     InputIcon)
+    .component('pv-input-text',     InputText)
+    .component('pv-input-number',   InputNumber)
+    .component('pv-menu',           Menu)
+    .component('pv-rating',         Rating)
+    .component('pv-row',            Row)
+    .component('pv-drawer',         Drawer)
+    .component('pv-tag',            Tag)
+    .component('pv-textarea',       Textarea)
+    .component('pv-toolbar',        Toolbar)
+    .component('pv-toast',          Toast);
+
+// Mount App
+app.mount('#app');
