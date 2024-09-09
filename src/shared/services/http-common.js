6e4cdd3686c25260377943d79b19b8cd13d1ad31
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-09 12:13:40 -0500

    chore: add axios instance default configuration.

---
+++ src/shared/services/http-common.js
@@ -0,0 +1,29 @@
+/**
+ * Axios Shareable Instance module
+ * @module shared/services/http-common
+ * @description
+ * http axios instance creation with default configs.
+ * @requires axios
+ * @requires env
+ * @exports http
+ */
+
+import axios from "axios";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+/**
+ * Axios instance
+ * @summary http axios instance creation with default configs
+ * @type {AxiosInstance}
+ *
+ */
+
+const http = axios.create({
+    baseURL: API_BASE_URL,
+});
+
+// Set default headers
+http.defaults.headers.common['Content-type']                = 'application/json';
+http.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
+export default http;
