0edd12f18347af25ad5fad96d75e3a9ef87fe727
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-13 18:02:01 -0500

    feat(categories): add category item create and edit dialog component.

---
+++ src/publishing/components/category-item-create-and-edit.component.vue
@@ -0,0 +1,45 @@
+<script>
+import CreateAndEdit from "../../shared/components/create-and-edit.component.vue";
+
+export default {
+  name: "category-item-create-and-edit-dialog",
+  components: {CreateAndEdit},
+  props: {
+    item: null,
+    visible: Boolean
+  },
+  data() {
+    return {
+      submitted: false
+    }
+  },
+  methods: {
+    onCanceledRequested() {
+      this.$emit('canceled');
+    },
+    onSaveRequested() {
+      this.submitted = true;
+      this.$emit('save', this.item);
+    }
+  }
+}
+</script>
+
+<template>
+  <create-and-edit :entity="item" :visible="visible" entity-name="Category" @cancelled="onCanceledRequested" @saved="onSaveRequested">
+    <template #content>
+      <div class="p-fluid">
+        <div class="field mt-5">
+          <pv-float-label>
+            <label for="name">Name</label>
+            <pv-input-text id="name" v-model="item.name" :class="{ 'p-invalid': submitted && !item.name }"/>
+          </pv-float-label>
+        </div>
+      </div>
+    </template>
+  </create-and-edit>
+</template>
+
+<style scoped>
+
+</style>
