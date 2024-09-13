490f396abf36462b4ce9bc769e05083cdfba8d37
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-13 11:41:04 -0500

    feat(in-app-navigation): setup initial router.

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@
  * Vue Router instance creation with default configs.
  */
 import {createRouter, createWebHistory} from "vue-router";
+import HomeComponent from "../public/pages/home.component.vue";
+import AboutComponent from "../public/pages/about.component.vue";
 
 /**
  * Vue Router instance
@@ -14,7 +16,17 @@ import {createRouter, createWebHistory} from "vue-router";
  */
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        { path: '/home',    name: 'home',   component: HomeComponent, meta: { title: 'Home' } },
+        { path: '/about',   name: 'about',  component: AboutComponent, meta: { title: 'About us' } },
+        { path: '/',        redirect: '/home' }
+    ]
+});
+
+router.beforeEach((to, from, next) => {
+    let baseTitle = 'ACME Learning Center';
+    document.title = `${baseTitle} | ${to.meta['title']}`;
+    next();
 });
 
 export default router;
