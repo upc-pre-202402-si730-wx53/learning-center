b7b1eb20cc275ebd6ffa34424a1e19ed40ca76cc
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-09-16 20:00:22 -0500

    chore: set app instance initial configuration

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import router from './routes/index.js';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// Router
+app.use(router);
+
+// PrimeVue
+app.use(PrimeVue, {ripple: true})
+
+// Mount App
+app.mount('#app');
