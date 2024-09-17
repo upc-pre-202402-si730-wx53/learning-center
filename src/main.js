afcc6e2e6e9bf4e20d19f6cd798f982fe90ffb24
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-09-16 20:26:06 -0500

    chore: setup primefles

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import router from './routes/index.js';
+import Dialog from "primevue/dialog";
+import Button from "primevue/button";
+
+// PrimeVue CSS
+import 'primeflex/primeflex.css';
 
 const app = createApp(App);
 
@@ -12,6 +17,8 @@ app.use(router);
 
 // PrimeVue
 app.use(PrimeVue, {ripple: true})
+    .component('pv-button', Button)
+    .component('pv-dialog', Dialog)
 
 // Mount App
 app.mount('#app');
