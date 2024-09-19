58f2e8a1f77c6e8af49933aa95a6af0305ac1228
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-09-18 19:26:43 -0500

    feat(i18n): setup i18n and add language switcher component

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 import router from './routes/index.js';
-import Dialog from "primevue/dialog";
-import Button from "primevue/button";
+import i18n from "@/i18n.js";
 
 // PrimeVue CSS
 import 'primeflex/primeflex.css';
@@ -14,6 +13,9 @@ import Toolbar from "primevue/toolbar";
 import ConfirmDialog from "primevue/confirmdialog";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
+import Dialog from "primevue/dialog";
+import Button from "primevue/button";
+import SelectButton from "primevue/selectbutton";
 
 const app = createApp(App);
 
@@ -27,8 +29,12 @@ app.use(PrimeVue, {ripple: true})
     .component('pv-column', Column)
     .component('pv-data-table', DataTable)
     .component('pv-dialog', Dialog)
+    .component('pv-select-button', SelectButton)
     .component('pv-toast', Toast)
     .component('pv-toolbar', Toolbar)
+
+// Vue i18n
+app.use(i18n);
 
 // Mount App
 app.mount('#app');
---
+++ src/public/components/language-switcher.component.vue
@@ -0,0 +1,22 @@
+<script>
+import PvSelectButton from "@/main.js";
+
+export default {
+  name: "language-switcher",
+  components: {PvSelectButton},
+  data(){
+    return {
+      languages: ['en', 'es'],
+      language: 'en'
+    }
+  }
+}
+</script>
+
+<template>
+  <pv-select-button v-model="$i18n.locale" :options="languages" class="uppercase" />
+</template>
+
+<style scoped>
+
+</style>
