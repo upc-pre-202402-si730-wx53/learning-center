25cd0713fef21bb34c4d86f6ddb55af13f7183fc
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-09-18 19:39:54 -0500

    feat(about): add about component

---
+++ src/public/pages/about.component.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "about",
+  title: "About",
+}
+</script>
+
+<template>
+  <div class="w-full align-content-center">
+    <h1>About</h1>
+    <p>ACME Learning Center is an Education Business Platform, part of ACME Corporation.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
